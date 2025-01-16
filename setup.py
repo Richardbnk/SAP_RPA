dba94a708163d37f225ed55c115ce2453b9ccb77
Richard Banak <45636763+Richardbnk@users.noreply.github.com>
2025-01-16 00:05:39 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name='sap_rpa',
+    version='1.0.0',
+    author="Richard Raphael Banak",
+    description="Library for scrapping and RPA in the SAP S3 and S/4HANA systems.",
+    url="https://github.com/Richardbnk/sap_rpa",
+    packages=['sap_rpa'],
+    
+    py_modules = ['PyPDF2'],
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=[required],
+)
